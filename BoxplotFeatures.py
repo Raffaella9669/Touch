b911f7ae2de5f02fb4945ca7fcba1d231d1c2f71
Andreana1 <32771888+Andreana1@users.noreply.github.com>
2020-05-26 20:08:08 +0200

    Update BoxplotFeatures.py

--- BoxplotFeatures.py
+++ BoxplotFeatures.py
@@ -3,26 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-
 users = pd.read_excel('keystroke_51_Aggiornato.xls', 'users')
 timers = pd.read_excel('keystroke_51_Aggiornato.xls', 'timers')
 
 utenti = timers.join(users.set_index('UserName'), on='UserName')
-#print(utenti.columns)
+# print(utenti.columns)
 print(utenti['Age'].value_counts())
 print(utenti)
 
 sns.boxplot(x='Age', y='SommaPP', data=utenti)
 
 plt.show()
-sns.boxplot(x='Age', y='SommaPR',data=utenti)
+sns.boxplot(x='Age', y='SommaPR', data=utenti)
 plt.show()
 
-sns.boxplot(x='Age', y='SommaRR',data=utenti)
+sns.boxplot(x='Age', y='SommaRR', data=utenti)
 plt.show()
 
-sns.boxplot(x='Age', y='SommaRP',data=utenti)
+sns.boxplot(x='Age', y='SommaRP', data=utenti)
 plt.show()
-
-
-
